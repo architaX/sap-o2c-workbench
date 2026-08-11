0a26ead063f2ea3a3c0215050d56dfe75d399184
architaX <architapanda43@gmail.com>
2026-08-11 09:07:33 +0530

    feat: initial SAP O2C workbench skeleton

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="SAP O2C Credit Workbench", layout="wide")
+st.title("SAP O2C Credit Management Workbench (VKM1)")
+st.info("System initializing... Core architecture ready for deployment.")
